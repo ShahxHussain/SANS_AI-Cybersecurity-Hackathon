df6b1054095b2a5177e0ee027313953bdab02ebf
mahadwajid <khandang510@gmail.com>
2025-03-15 15:58:23 +0500

    Update

--- Server/index.js
+++ Server/index.js
@@ -2,21 +2,28 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./Connection.js"; // Ensure this matches the export in Connection.js
+import connectDB from "./Connection.js";
 import datasetRoutes from "./Routes/datasetRoutes.js";
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-
 // Connect to MongoDB
 connectDB();
 
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:3000', // Allow your client's origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true,
+};
+
+app.use(cors(corsOptions)); // Apply CORS middleware with options
+
 // Middleware
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,8 +31,7 @@ app.use(express.json());
 // Serve routes
 app.use("/datasets", datasetRoutes);
 
-
-// Start ServeZ
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
