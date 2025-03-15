92c54405e9e1e72d9eab9c2200f382473b1085d7
mahadwajid <khandang510@gmail.com>
2025-03-15 15:10:13 +0500

    Update App.js

--- intrusion-detection/src/App.js
+++ intrusion-detection/src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
-import Login from "./Pages/Login";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./Pages/Main";
-import { Route, Routes } from "react-router-dom";
-import Signup from "./Pages/Signup";
 
 function App() {
   return (
- 
- <Routes>
-   {/* <Route path="/"  element={<Login />} />
-   <Route path="/signup"  element={<Signup />} /> */}
-    <Route path="/Main/*"  element={<Main />}/> 
- </Routes>
-  
+    <Routes>
+      {/* Redirect the root path to /Main */}
+      <Route path="/" element={<Navigate to="/Main" replace />} />
+      <Route path="/Main/*" element={<Main />} />
+    </Routes>
   );
 }
 
